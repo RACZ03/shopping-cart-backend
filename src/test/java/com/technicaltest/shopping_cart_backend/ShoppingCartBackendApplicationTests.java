dca8fa293dac00bbe56aabde616ae89fd2227fbc
Roberto Calero <rmorck03@gmail.com>
2025-02-24 21:26:29 -0600

    chore: 🎉 Initial project configuration commit - Setup Maven multi-module project with Spring Boot, Java 21, and required dependencies

---
+++ src/test/java/com/technicaltest/shopping_cart_backend/ShoppingCartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicaltest.shopping_cart_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
